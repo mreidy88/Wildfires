be74a4f1eea1f0d23924de0dc8eb4bbe31b19c8b
Mike Reidy <mreidy88@gmail.com>
2020-12-03 16:23:23 -0500

    Wildfire app with errors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Map from './Components/Map';
+import LocationMarker from './Components/LocationMarker';
+import Loader from './Components/Loader';
+import Header from './Components/Header';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [eventData, setEventData] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setLoading(true)
+      const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events')
+      const { events } = res.json()
+
+      setEventData(events)
+      setLoading(false)
+    }
+
+    fetchEvents()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      { !loading ? <Map eventData={eventData} /> : <Loader /> }
+
     </div>
   );
 }
